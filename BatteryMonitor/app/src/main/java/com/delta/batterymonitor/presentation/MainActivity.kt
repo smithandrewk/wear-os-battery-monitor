214b545a03c5f7c2b44c8228ea4a279110458786
Andrew Smith <andrewsmith@sc.edu>
2023-09-27 18:41:06 -0400

    register accelerometer

--- BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/MainActivity.kt
+++ BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.delta.batterymonitor.presentation
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
@@ -8,12 +10,15 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     private lateinit var mFileHandler: FileHandler
     private lateinit var mBatteryHandler: BatteryHandler
+    private lateinit var mSensorHandler: SensorHandler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         mFileHandler = FileHandler(getExternalFilesDir(null)!!)
         mBatteryHandler = BatteryHandler(::registerReceiver,::unregisterReceiver, mFileHandler)
+        mSensorHandler = SensorHandler(mFileHandler,getSystemService(SENSOR_SERVICE) as SensorManager)
 
         setContent {
             WearApp("Android")
---
+++ BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/SensorHandler.kt
@@ -0,0 +1,26 @@
+package com.delta.batterymonitor.presentation
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+
+class SensorHandler(fileHandler: FileHandler, sensorManager: SensorManager) : SensorEventListener {
+    private var mSensorManager: SensorManager = sensorManager
+    private var mFileHandler: FileHandler = fileHandler
+
+    init {
+        val samplingRateHertz = 100
+        val mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val samplingPeriodMicroseconds = 1000000/samplingRateHertz
+        mSensorManager.registerListener(this, mAccelerometer, samplingPeriodMicroseconds)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {}
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+    fun unregister() {
+        mSensorManager.unregisterListener(this)
+    }
+}
