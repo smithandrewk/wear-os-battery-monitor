7a19fb884b132b30a469d27c9fe431e48a2c30e5
Andrew Smith <andrewsmith@sc.edu>
2023-09-28 15:08:34 -0400

    abstract accelerometer listener

--- BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/SensorHandler.kt
+++ BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/SensorHandler.kt
@@ -5,24 +5,17 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 
-class SensorHandler(fileHandler: FileHandler, sensorManager: SensorManager) : SensorEventListener {
+class SensorHandler(fileHandler: FileHandler, sensorManager: SensorManager) {
     private var mSensorManager: SensorManager = sensorManager
     private var mFileHandler: FileHandler = fileHandler
-
+    private val mAccelerometerListener: AccelerometerListener = AccelerometerListener(mFileHandler)
     init {
         val samplingRateHertz = 100
         val mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         val samplingPeriodMicroseconds = 1000000/samplingRateHertz
-        mSensorManager.registerListener(this, mAccelerometer, samplingPeriodMicroseconds)
+        mSensorManager.registerListener(mAccelerometerListener, mAccelerometer, samplingPeriodMicroseconds)
     }
-
-    override fun onSensorChanged(event: SensorEvent) {
-        mFileHandler.writeAccelerometerEvent(event)
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
-
-    fun unregister() {
-        mSensorManager.unregisterListener(this)
+    fun unregisterAccelerometer() {
+        mSensorManager.unregisterListener(mAccelerometerListener)
     }
 }
