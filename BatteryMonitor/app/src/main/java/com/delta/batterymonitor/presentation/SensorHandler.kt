9c228cc9d6fef72505b9ce9292397e68b143ded0
Andrew Smith <andrewsmith@sc.edu>
2023-10-05 10:40:34 -0400

    register gyro

--- BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/SensorHandler.kt
+++ BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/SensorHandler.kt
@@ -9,11 +9,14 @@ class SensorHandler(fileHandler: FileHandler, sensorManager: SensorManager) {
     private var mSensorManager: SensorManager = sensorManager
     private var mFileHandler: FileHandler = fileHandler
     private val mAccelerometerListener: AccelerometerListener = AccelerometerListener(mFileHandler)
+    private val mGyroscopeListener: GyroscopeListener = GyroscopeListener(mFileHandler)
     init {
         val samplingRateHertz = 100
         val mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val mGyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         val samplingPeriodMicroseconds = 1000000/samplingRateHertz
         mSensorManager.registerListener(mAccelerometerListener, mAccelerometer, samplingPeriodMicroseconds)
+        mSensorManager.registerListener(mGyroscopeListener, mGyroscope, samplingPeriodMicroseconds)
     }
     fun unregisterAccelerometer() {
         mSensorManager.unregisterListener(mAccelerometerListener)
