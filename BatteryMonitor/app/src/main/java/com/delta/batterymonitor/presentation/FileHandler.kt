6eb0835c5f24cff391acf71328d70cf6273bb9e1
Andrew Smith <andrewsmith@sc.edu>
2023-10-04 20:50:59 -0400

    add abstracted gyro

--- BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/FileHandler.kt
+++ BatteryMonitor/app/src/main/java/com/delta/batterymonitor/presentation/FileHandler.kt
@@ -14,17 +14,23 @@ class FileHandler (filesDir: File) {
     )
     private var fBatteryLog: FileOutputStream
     private var fAccelerometerData: FileOutputStream
+    private var fGyroscopeData: FileOutputStream
     private val mFilesDir = filesDir
     init {
         File(mFilesDir, appStartTimeReadable).mkdir()
         fBatteryLog = FileOutputStream(File(filesDir, "$appStartTimeReadable/battery.csv"))
         fAccelerometerData = FileOutputStream(File(filesDir, "$appStartTimeReadable/acceleration.csv"))
         fAccelerometerData.write("timestamp,x,y,z\n".toByteArray())
+        fGyroscopeData = FileOutputStream(File(filesDir, "$appStartTimeReadable/gyroscope.csv"))
+        fGyroscopeData.write("timestamp,x,y,z\n".toByteArray())
     }
     fun writeToLog(msg:String){
         fBatteryLog.write("${Calendar.getInstance().timeInMillis},$msg\n".toByteArray())
     }
     fun writeAccelerometerEvent(event: SensorEvent){
         fAccelerometerData.write("${event.timestamp},${event.values[0]},${event.values[1]},${event.values[2]}\n".toByteArray())
+    }
+    fun writeGyroscopeEvent(event: SensorEvent){
+        fGyroscopeData.write("${event.timestamp},${event.values[0]},${event.values[1]},${event.values[2]}\n".toByteArray())
     }
 }
